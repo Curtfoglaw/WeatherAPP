05076d44407fbcdb37449f8afa4d570d447136e1
Curtfoglaw <curtlawrence1@gmail.com>
2026-06-12 16:39:56 +1000

    Finally connected up my backend to the HTML to render results from API requests

--- WeatherApp.py
+++ WeatherApp.py
@@ -5,32 +5,30 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def WeatherApp():
+
+    city_name = None
+    city_condition = None
+    city_temp_celsius = None
+    city_temp_feels_like = None
     
-    return render_template("WeatherApp.html")
     load_dotenv()
     API_KEY = os.getenv("API_KEY")
+
+    if request.method == "POST":
+        city = request.form.get("city")
+        API_URL_CURR_WEATHER = f"https://api.weatherapi.com/v1/current.json?key={API_KEY}&q={city}"
+        response_crr_weather = requests.get(API_URL_CURR_WEATHER)
+
+        if response_crr_weather.status_code != 200:
+            return "Request failed"
+
+        data_crr_weather = response_crr_weather.json()
+        city_name = data_crr_weather['location']['name']
+        city_condition = data_crr_weather['current']['condition']['text']
+        city_temp_celsius = data_crr_weather['current']['temp_c']
+        city_temp_feels_like = data_crr_weather['current']['feelslike_c']
+
+    return render_template("WeatherApp.html", city_name=city_name, city_condition=city_condition, city_temp_celsius=city_temp_celsius, city_temp_feels_like=city_temp_feels_like)
     
-    city = input("What city would you like to view the weather for today?: ").capitalize()
-
-    API_URL_CURR_WEATHER = f"https://api.weatherapi.com/v1/current.json?key={API_KEY}&q={city}"
-    response_crr_weather = requests.get(API_URL_CURR_WEATHER)
-
-    if response_crr_weather.status_code != 200:
-        return "Request failed"
-
-    data_crr_weather = response_crr_weather.json()
-    city_name = data_crr_weather['location']['name']
-    city_condition = data_crr_weather['current']['condition']['text']
-    city_temp_celsius = data_crr_weather['current']['temp_c']
-    city_temp_feels_like = data_crr_weather['current']['feelslike_c']
-
-
-    """print(data_crr_weather)"""
-    print(f"City: {city_name}\n")
-    print(f"Condition: {city_condition}\n")
-    print(f"Temperature (C): {city_temp_celsius}\n")
-    print(f"Temperature feels like (C): {city_temp_feels_like}\n")
-
-    print(data_crr_weather)
