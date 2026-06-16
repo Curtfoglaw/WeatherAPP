c37dd30a11f4b8ccd2282eee749d5cfa71c84d83
Curtfoglaw <curtlawrence1@gmail.com>
2026-06-16 13:46:19 +1000

    Started on a way for logged in users to add weather to their own page

--- WeatherApp.py
+++ WeatherApp.py
@@ -6,6 +6,8 @@ import requests
 import os
 from dotenv import load_dotenv
 
+
+#configure imports for database, environment variables, login management and Flask
 app = Flask(__name__)
 
 load_dotenv()
@@ -17,20 +19,34 @@ app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 db = SQLAlchemy(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
-login_manager.login_view = "login"
+login_manager.login_view = "Login"
 
+
+#class for creating a user table in the dataabse
 class Users(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(250), unique=True, nullable=False)
     password = db.Column(db.String(250), nullable = False)
+    weather_entries = db.relationship("WeatherHistory", backref="user", lazy=True)
+
+class WeatherHistory(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    city = db.Column(db.String(250))
+    condition = db.Column(db.String(250))
+    temp_celsius = db.Column(db.Float)
+    temp_feels_like = db.Column(db.Float)
 
 with app.app_context():
     db.create_all()
 
+#login manager
 @login_manager.user_loader
 def load_user(user_id):
     return Users.query.get(int(user_id))
 
+
+#routing for different pages + API imports
 @app.route("/")
 def index():
     return render_template("HomePage.html")
@@ -74,6 +90,12 @@ def Login():
 def Dashboard():
     return render_template("Dashboard.html", user=current_user.username)
 
+@app.route("/Logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("/"))
+
 @app.route("/WeatherApp", methods=["GET", "POST"])
 def WeatherApp():
 
@@ -101,6 +123,46 @@ def WeatherApp():
         city_temp_feels_like = data_crr_weather['current']['feelslike_c']
 
     return render_template("WeatherApp.html", city_name=city_name, city_condition=city_condition, city_temp_celsius=city_temp_celsius, city_temp_feels_like=city_temp_feels_like)
+
+@app.route("/WeatherAppLoggedIn", methods=["GET", "POST"])
+@login_required
+def WeatherAppLoggedIn():
+
+    city_name = None
+    city_condition = None
+    city_temp_celsius = None
+    city_temp_feels_like = None
+    error_message = None
+    weather_history = WeatherHistory.query.filter_by(user_id = current_user.id).all()
+
+    API_KEY = os.getenv("API_KEY")
+
+    if request.method == "POST":
+        city = request.form.get("city")
+        API_URL_CURR_WEATHER = f"https://api.weatherapi.com/v1/current.json?key={API_KEY}&q={city}"
+        response_crr_weather = requests.get(API_URL_CURR_WEATHER)
+
+        if response_crr_weather.status_code != 200:
+            error_message = "Please enter a valid city name"
+            return render_template("WeatherAppLoggedIn.html", error_message=error_message, history=weather_history) 
+        
+        data_crr_weather = response_crr_weather.json()
+        city_name = data_crr_weather['location']['name']
+        city_condition = data_crr_weather['current']['condition']['text']
+        city_temp_celsius = data_crr_weather['current']['temp_c']
+        city_temp_feels_like = data_crr_weather['current']['feelslike_c']
+
+        new_entry = WeatherHistory(user_id = current_user.id, city = city_name, condition = city_condition, temp_celsius = city_temp_celsius, temp_feels_like = city_temp_feels_like)
+        db.session.add(new_entry)
+        db.session.commit()
+        weather_history = WeatherHistory.query.filter_by(user_id = current_user.id).all()
+
+
+
+
+    return render_template("WeatherAppLoggedIn.html", city_name=city_name, city_condition=city_condition, city_temp_celsius=city_temp_celsius, city_temp_feels_like=city_temp_feels_like, history=weather_history, current_user_name = current_user.username, error_message=error_message)
+        
+
     
 if __name__ == "__main__":
     app.run(debug=True)
